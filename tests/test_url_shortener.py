8539e9b5ab020ae90c195baede992d919ae120c9
Lesther Jr Llacuna <ljllacuna5@gmail.com>
2024-01-26 15:24:14 +0000

    Add more tests

--- tests/test_url_shortener.py
+++ tests/test_url_shortener.py
@@ -1,47 +1,4 @@
-from contextlib import asynccontextmanager
-
-import pytest
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
-
-from url_shortener.base import init_models
-from url_shortener.dependencies import get_session
-from url_shortener.main import app
-from url_shortener.settings import SETTINGS
-
-test_engine = create_async_engine(
-    SETTINGS.test_db_dsn,
-    echo=True,
-    # poolclass=StaticPool,
-)
-
-TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=test_engine, class_=AsyncSession
-)
-
-
-async def override_get_session() -> AsyncSession:
-    await init_models(engine=test_engine)
-    async with TestingSessionLocal() as session:
-        yield session
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # await init_models(engine=test_engine)
-    yield
-
-
-app.dependency_overrides[get_session] = override_get_session
-app.lifespan = lifespan
-
-
-@pytest.fixture(scope="session")
-def client():
-    return TestClient(app)
 
 
 def test_index(client: TestClient):
@@ -50,14 +7,48 @@ def test_index(client: TestClient):
     assert response.json() == "pong"
 
 
-def test_create_short_url_route(client: TestClient):
+def test_empty_urls(client: TestClient):
     response = client.get("/urls/")
 
     assert response.status_code == 200
     assert response.json() == []
 
+
+def test_create_one_short_url(client: TestClient):
     response = client.post("/urls/", json={"long_url": "https://google.com"})
 
     assert response.status_code == 200
     assert response.json()["long_url"] == "https://google.com"
     assert response.json()["short_url"] == "1"
+
+
+def test_create_two_short_url(client: TestClient):
+    response = client.post("/urls/", json={"long_url": "https://google.com"})
+
+    assert response.status_code == 200
+    assert response.json()["long_url"] == "https://google.com"
+    assert response.json()["short_url"] == "1"
+
+
+def test_non_existent_url(client: TestClient):
+    response = client.get("/abcdef")
+    assert response.status_code == 404
+
+
+def test_get_non_existent_short_url(client: TestClient):
+    short_url = "non-existent"
+    response = client.get(f"/urls/{short_url}")
+    assert response.status_code == 404
+
+
+def test_create_and_check_short_url(client: TestClient):
+    response = client.post("/urls/", json={"long_url": "https://google.com"})
+
+    assert response.status_code == 200
+    assert response.json()["long_url"] == "https://google.com"
+    assert response.json()["short_url"] == "1"
+
+    short_url = response.json()["short_url"]
+    response = client.get(f"/urls/{short_url}")
+
+    assert response.json()["short_url"] == short_url
