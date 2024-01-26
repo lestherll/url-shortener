302c734cfc14a804b0d21e712680010a153b8d81
Lesther Jr Llacuna <ljllacuna5@gmail.com>
2024-01-26 15:22:53 +0000

    Update session dependency

--- url_shortener/dependencies.py
+++ url_shortener/dependencies.py
@@ -4,5 +4,5 @@ from url_shortener.base import async_session
 
 
 async def get_session() -> AsyncSession:
-    async with async_session() as session:
+    async with async_session.begin() as session:
         yield session
