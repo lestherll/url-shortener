a72406e62548f9d87f10682c25613a503bd30beb
Lesther Jr Llacuna <ljllacuna5@gmail.com>
2024-01-30 01:19:04 +0000

    Update cache config

--- url_shortener/cache.py
+++ url_shortener/cache.py
@@ -3,4 +3,4 @@ from pymemcache.client import base
 
 from url_shortener.settings import SETTINGS
 
-cache = base.Client(SETTINGS.cache_dsn, timeout=5, serde=serde.pickle_serde)
+cache = base.Client(SETTINGS.cache_dsn, connect_timeout=5, timeout=5, serde=serde.pickle_serde, ignore_exc=True)
